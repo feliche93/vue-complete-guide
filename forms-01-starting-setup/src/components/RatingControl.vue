43b005ee1e589ee5ea9fb1a808d5e17d0952369a
Felix Vemmer <felix.vemmer@gmail.com>
2021-07-05 21:09:33 +0200

    feat: forms

---
+++ forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,65 @@
+<template lang="">
+    <div>
+        <ul>
+            <li :class="{active: modelValue === 'poor'}"><button type="button" @click="activate('poor')">Poor</button></li>
+            <li :class="{active: modelValue === 'average'}"><button type="button" @click="activate('average')">Average</button></li>
+            <li :class="{active: modelValue === 'great'}"><button type="button" @click="activate('great')">Great</button></li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: ['modelValue'],
+    emitss: ['update:modelValue'],
+//   data() {
+//     return {
+//         activeOption: this.modelValue
+//     };
+//   },
+// computed: {
+//     activeOption() {
+//         return this.mod
+//     }
+// },
+  methods: {
+      activate(option) {
+          this.$emit('update:modelValue', option)
+      }
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: blue;
+}
+
+.active button {
+  color: blue;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+}
+</style>
